e8983f0627245ba1a3c20615d0f2a4d6e2ae7870
Namish bhargav <98833850+namish01@users.noreply.github.com>
2024-01-06 18:48:27 +0000

    let,var,const,scoping,console through table

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,12 @@
+const accountId=1441
+let accountEmail="namishbhargav121"
+var accountPassword=1234
+accountCity="dehradun"
+
+let accountState;
+
+accountEmail="Namishbhargav121"
+accountPassword=1224
+accountCity="herbertpur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
