64f64ddf9e23199cf184f61769bad94baf7afd5b
Namish bhargav <98833850+namish01@users.noreply.github.com>
2024-01-11 11:19:33 +0000

    just have a look on the datatypes present in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let age=21;
+let isloggedIn=true;
+let state;
+
+// Number
+// String=""
+// Boolean=true/false
+// bigint
+// null
+// undefined
+// 
+
+// objects
+
+console.log(typeof (null)) //object
+console.log(typeof (undefined)) //undefined
