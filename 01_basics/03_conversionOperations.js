abe525269185c287cd1eead1ca10fe2100c9950c
Namish bhargav <98833850+namish01@users.noreply.github.com>
2024-01-11 11:20:41 +0000

    How the conversions happen and their actual value after conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+// let score=33
+let score="33"
+// let score="33abc"
+// let score=true
+
+// console.log(typeof score)
+// let scoreInNumber=Number(score)
+// console.log(typeof scoreInNumber)
+// console.log(scoreInNumber)
+
+// 33=>Number
+// "33"=>33
+// "33abs"=>NaN
+// true=>1,false=>0
+
+
+// let name="namish";
+let name=""
+console.log(typeof (name))
+let nameInBoolean=Boolean(name)
+console.log(typeof nameInBoolean)
+console.log(nameInBoolean)
+
+// "namish"=>true;
+// ""=>false
+
